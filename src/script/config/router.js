57126d2ec2e2e5257fdb8d260e0a75d9c1cb7e74
z00294266 <zs.zhangshuai@huawei.com>
2018-01-13 18:32:00 +0800

    update

--- src/script/config/router.js
+++ src/script/config/router.js
@@ -4,6 +4,10 @@ angular.module('app').config(['$stateProvider', '$urlRouterProvider', function($
         url: '/main',
         templateUrl: 'view/main.html',
         controller: 'mainCtrl'
+    }).state('company',{
+        url:"/company/:id",
+        templateUrl:"view/template/company.html",
+        controller:"companyCtrl"
     });
     $urlRouterProvider.otherwise('main');
 }]);
--- src/script/controller/mainCtrl.js
+++ src/script/controller/mainCtrl.js
@@ -1,4 +1,53 @@
 'use strict';
 angular.module('app').controller('mainCtrl',['$scope',function ($scope) {
-    console.log("dddddddd")
+    $scope.list1=[{
+        id:'1232',
+        name:'销售',
+        imgSrc:'image/company-1.png',
+        companyName:'千度',
+        city:'上海',
+        industry:'互联网',
+        time:'2017-01-23 12:33'
+    },{
+        id:'1234',
+        name:'PHP',
+        imgSrc:'image/company-2.png',
+        companyName:'阿里巴巴',
+        city:'深圳',
+        industry:'互联网',
+        time:'2017-03-23 12:44'
+    },{
+        id:'1235',
+        name:'Java',
+        imgSrc:'image/company-3.png',
+        companyName:'百度',
+        city:'深圳',
+        industry:'互联网',
+        time:'2017-05-23 10:42'
+    }]
+    $scope.list2=[{
+        id:'1232',
+        name:'销售',
+        imgSrc:'image/company-1.png',
+        companyName:'千度',
+        city:'上海',
+        industry:'互联网',
+        time:'2017-01-23 12:33'
+    },{
+        id:'1234',
+        name:'PHP',
+        imgSrc:'image/company-2.png',
+        companyName:'阿里巴巴',
+        city:'深圳',
+        industry:'互联网',
+        time:'2017-03-23 12:44'
+    },{
+        id:'1235',
+        name:'Java',
+        imgSrc:'image/company-3.png',
+        companyName:'百度',
+        city:'深圳',
+        industry:'互联网',
+        time:'2017-05-23 10:42'
+    }]
 }]);
